7182f02e1c5b2df8b5e7cc2b64adff6fd5e41695
Meghana <meghana1227@gmail.com>
2019-02-28 23:07:10 +0530

    added aeroplane image to aeroplane.js component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Container, Row, Col } from 'react-grid-system';
 import Rocket from './components/Rocket';
 import Cloud from './components/Cloud';
 import Bird from './components/Bird';
+import Aeroplane from './components/Aeroplane';
 class App extends Component {
   render() {
     return (
@@ -10,14 +11,17 @@ class App extends Component {
         
   <Container>
   <Row>
-    <Col sm={4} xs={12} md={4} >
+    <Col sm={3} xs={12} md={3} >
         <Bird />
     </Col>
-    <Col sm={4} xs={12} md={4}> 
+    <Col sm={3} xs={12} md={3}> 
         <Rocket />
     </Col>
-    <Col sm={4} xs={12} md={4}>
+    <Col sm={3} xs={12} md={3}>
       <Cloud />
+    </Col>
+    <Col sm={3} xs={12} md={3} >
+        <Aeroplane />
     </Col>
   </Row>
   </Container>
