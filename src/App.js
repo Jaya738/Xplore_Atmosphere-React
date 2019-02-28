53821ce206a80ee2f15fb981718151ca4b3290a2
Meghana <meghana1227@gmail.com>
2019-03-01 00:49:12 +0530

    added satellite image to satellite.js component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Container, Row, Col } from 'react-grid-system';
 import Rocket from './components/Rocket';
 import Cloud from './components/Cloud';
-import Bird from './components/Bird';
 import Aeroplane from './components/Aeroplane';
 import Satellite from './components/Satellite';
 
@@ -13,19 +12,17 @@ class App extends Component {
         
   <Container>
   <Row>
-    <Col sm={3} xs={12} md={3} >
-        <Bird />
-    </Col>
-    <Col sm={3} xs={12} md={3}> 
+   
+    <Col sm={4} xs={12} md={3}> 
         <Rocket />
     </Col>
-    <Col sm={3} xs={12} md={3}>
+    <Col sm={4} xs={12} md={3}>
       <Cloud />
     </Col>
-    <Col sm={3} xs={12} md={3} >
+    <Col sm={12} xs={12} md={3} >
         <Aeroplane />
     </Col>
-    <Col sm={2} xs={4} md={4}>
+    <Col sm={12} xs={4} md={4}>
        <Satellite/>
     </Col>
     
--- src/components/Satellite.js
+++ src/components/Satellite.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+
 import satellite from './images/satellite.jpg';
 class Satellite extends Component {
     render() {
         return (
-        <img src={satellite} alt="Satellite " />
+        <img src={satellite} alt="Satellite" />
         );
     }
 }
