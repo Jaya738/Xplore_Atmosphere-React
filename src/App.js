c894d315802cdb9e76541f7efdbcff260b8e522c
Your Name <praneethnersu@gmail.com>
2019-03-29 12:25:07 +0530

    fixed error in react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'react-grid-system';
-import { BrowserRouter as Switch, Route, Redirect } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Tutorial from './components/Tutorial';
 import Troposphere from './components/Troposphere.js';
 import Stratosphere from './components/Stratosphere.js';
@@ -13,29 +13,29 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <div>
+      <Row>
+      <Col md={10}>
+
           <Switch>
-               <div>
-                 <Row>
-                <Col>  
+                
                   <Route path="/home" component={Home} />
                   <Route path="/exosphere" component={Exosphere} />
                   <Route path="/thermosphere" component={Thermosphere} />
                   <Route path="/mesosphere" component={Mesosphere} />
                   <Route path="/stratosphere" component={Stratosphere} />
                   <Route path="/troposphere" component={Troposphere} />
-                  <Route path="/tutorial" component={Tutorial} />
-                  <Redirect to="/tutorial" />
-                </Col>   
-                <Col>
-                <p>
-                  Hello
-                  </p></Col>         
-                  </Row>
-
-               </div>
-     </Switch>  
+                  <Redirect to="/home" />
+                
+     </Switch> 
+     </Col>
+     <Col md={2}>
+        <p>Buttons</p>
+     </Col>
+     </Row>
       </div>
+      </BrowserRouter>
         
     );
   }
