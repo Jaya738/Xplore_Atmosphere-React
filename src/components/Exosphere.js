017c38ca27e23f1e5a65c1f58744080b15df5c1e
afridikhan007 <afridikhanrockz@gmail.com>
2019-03-30 23:01:31 +0530

    added buttons logos in images

--- src/components/Exosphere.js
+++ src/components/Exosphere.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'react-grid-system';
 import xosphere from './reusable_components/images/exosphere.jpg';
+import rocket from './reusable_components/images/rocket.jpg';
 import "./exo.css"
 class Exosphere extends Component {
     render() {
         return (
             
                <Row className="exosphere" style={{backgroundImage:'url(' + xosphere + ')'}}>
-
+                <Col md={5}>
+                <img src={rocket} className="rocket"/>
+                </Col>
        </Row>  
    
         );
