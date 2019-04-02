d7fb826300e56abd1e06adfd273e13db5e47d09a
afridikhan007 <afridikhanrockz@gmail.com>
2019-04-02 18:22:15 +0530

    thermosphere layer

--- src/components/Thermosphere.js
+++ src/components/Thermosphere.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'react-grid-system';
 import Rocket from './reusable_components/Rocket.js';
-
+import thrsphere from './reusable_components/images/mesosphere.jpg';
+import "./exo.css"
 class Thermosphere extends Component {
     render() {
         return (
-         <Row>
-                
-                <Col md={5} >
+         <Row  className="th" style={{backgroundImage:'url(' + thrsphere + ')'}}>
+                 
+        <Col md={5} >
                      <Row align="center" style={{ height: '100px' }}>
                        <Col md={12} style={{backgroundColor: "red"}}><p>Thermosphere data</p></Col>
                        
