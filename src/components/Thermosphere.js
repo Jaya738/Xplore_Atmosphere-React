8561907d97081a77afe7757b06303223a8a44b96
guptamkumar <boniguptamkumar@gmail.com>
2019-04-25 15:18:19 +0530

    Report for Interactive Simulations Using Web Apps For Digital Education

--- src/components/Thermosphere.js
+++ src/components/Thermosphere.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'react-grid-system';
 import Rocket from './reusable_components/Rocket.js';
-import thrsphere from './reusable_components/images/thermosphere.jpeg';
+import thermosphere from './reusable_components/images/thermosphere.jpg';
 import "./thermo.css";
 
 
 class Thermosphere extends Component {
     render() {
         return (
-        <div> <Row>
+        <div> 
+                     <Row  className="thermosphere" style={{backgroundImage:'url(' + thermosphere + ')'}}>
+
              <Col md={5}><Container><p>c1</p></Container></Col>
              <Col md={2}><Container><Row><Col md={15}><Rocket /></Col></Row></Container></Col>
              <Col md={5}><Container><p>c2</p></Container></Col>
