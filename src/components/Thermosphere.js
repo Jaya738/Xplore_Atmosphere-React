5e7970214cee5483bf6d77b72ecfae008a99d1e8
harshitha.vinya <harshitha.vinya@gmail.com>
2019-04-26 22:05:32 +0530

    Balaswecha presentation

--- src/components/Thermosphere.js
+++ src/components/Thermosphere.js
@@ -1,28 +1,15 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'react-grid-system';
 import Rocket from './reusable_components/Rocket.js';
-<<<<<<< HEAD
 
 class Thermosphere extends Component {
     render() {
         return (
-        <div> <Row>
-             <Col md={5}><Container><p>c1</p></Container></Col>
-             <Col md={2}><Container><Row><Col md={15}><Rocket /></Col></Row></Container></Col>
-             <Col md={5}><Container><p>c2</p></Container></Col>
-            </Row>
-         </div>
-=======
-import thrsphere from './reusable_components/images/mesosphere.jpg';
-import "./exo.css"
-class Thermosphere extends Component {
-    render() {
-        return (
-         <Row  className="th" style={{backgroundImage:'url(' + thrsphere + ')'}}>
-                 
-        <Col md={5} >
+              <Row>
+                
+                <Col md={5} >
                      <Row align="center" style={{ height: '100px' }}>
-                       <Col md={12} style={{backgroundColor: "red"}}><p>Thermosphere data</p></Col>
+                       <Col md={12} style={{backgroundColor: "red"}}><p>Troposphere data</p></Col>
                        
                      </Row>
                 </Col>
@@ -39,13 +26,14 @@ class Thermosphere extends Component {
 
                 <Col md={5}>
                      <Row align="center" style={{ height: '100px' }}>
-                      <Col md={12} style={{backgroundColor: "red"}}><p>Thermosphere data</p></Col>
+                      <Col md={12} style={{backgroundColor: "red"}}><p>Troposphere data</p></Col>
                       
                     </Row>
                 </Col>
              
               </Row> 
->>>>>>> 4faadd710baf9ff93f85b3f0893d0ffc90da2558
+              
+            
         );
     }
 }
